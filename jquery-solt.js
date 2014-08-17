509964fd899e9361e687580dc2610385ecde417d
Satow <t.satow.solt@gmail.com>
2014-08-17 14:46:50 +0900

    20140816

---
+++ jquery-solt.js
@@ -0,0 +1,9 @@
+/*!
+ * jQuery Solt Plugin 1.0.0pre
+ * http://github.com/Satow/solt-js.git
+ * Requires jQuery 1.4.2
+ * 
+ * Copyright 2014, t.satow.solt@gmail.com.
+ */
+(function($) {
+})(jQuery);
