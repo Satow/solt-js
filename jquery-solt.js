22a6cbdf39d9aa1cd40d23282a516c0e239d4163
Satow <t.satow.solt@gmail.com>
2014-08-17 14:47:06 +0900

    Revert "20140816"
    This reverts commit 509964fd899e9361e687580dc2610385ecde417d.

--- jquery-solt.js
+++
@@ -1,9 +0,0 @@
-/*!
- * jQuery Solt Plugin 1.0.0pre
- * http://github.com/Satow/solt-js.git
- * Requires jQuery 1.4.2
- * 
- * Copyright 2014, t.satow.solt@gmail.com.
- */
-(function($) {
-})(jQuery);
